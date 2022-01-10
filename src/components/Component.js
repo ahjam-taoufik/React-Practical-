2c15fbfcbdf7bbc9cc2e4ff8701a0d2d02d2362a
taoufik ahjam <taoufikahjam@gmail.com>
2022-01-10 13:48:26 +0100

    tooltip

--- src/components/Component.js
+++ src/components/Component.js
@@ -1,56 +1,12 @@
+import Tippy from "@tippyjs/react";
+import "tippy.js/dist/tippy.css"; // optional
 
-import React from "react";
-import Modal from "react-modal";
-
-const customStyles = {
-  content: {
-    top: "50%",
-    left: "50%",
-    right: "auto",
-    bottom: "auto",
-    marginRight: "-50%",
-    transform: "translate(-50%, -50%)",
-    color:'red'
-   
-  },
-  overlay:{
-    backgroundColor:'grey'
-  }
-};
-
-Modal.setAppElement('#root');
 const Component = () => {
-  //==================================
-  const [modalIsOpen, setIsOpen] = React.useState(false);
-
-  function openModal() {
-    setIsOpen(true);
-  }
-
-  function closeModal() {
-    setIsOpen(false);
-  }
-
-  //==================================
-
   return (
     <div>
-      <button onClick={openModal}>Open Modal</button>
-      <Modal
-        isOpen={modalIsOpen}
-      
-        onRequestClose={closeModal}
-        style={customStyles}
-        contentLabel="Example Modal"
-      >
-        <h2>Hello</h2>
-        <button onClick={closeModal}>close</button>
-        <div>I am a modal</div>
-        <form>
-          <input />
-          <button>tab navigation</button>
-        </form>
-      </Modal>
+      <Tippy content="Hello I am tooltip">
+        <button>My button</button>
+      </Tippy>
     </div>
   );
 };
